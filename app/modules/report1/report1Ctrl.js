3f29db484d4e8c3361274634c580ddad7826eb5f
Andre Agunzi <aagunzi@gmail.com>
2017-09-11 23:34:55 -0300

    relatorio beta 1.0

--- app/modules/report1/report1Ctrl.js
+++ app/modules/report1/report1Ctrl.js
@@ -32,7 +32,7 @@
 			vm.query = {
 			  sort: 'nome',
 			  order: 'nome',
-			  q: 'edson facholi',
+			  // q: 'edson facholi',
 			  limit: 30,
 			  page: 1
 			};
@@ -43,6 +43,8 @@
 
 			  vm.contribs = response;
 
+			  vm.ano = 2017;
+
 			  response.$promise.then(function(greeting) {
 					  // console.log('Success: ' + greeting);
 					  var pay = new Array;
@@ -50,9 +52,21 @@
 						       if (greeting.hasOwnProperty(key)) {
 						       		if (greeting[key].id)
 						       		{
-						              console.log(greeting[key].id);
+						              // console.log(greeting[key].id);
 
 						              vm.meses[greeting[key].id] = [];
+						              vm.meses[greeting[key].id].jan = 0;
+						              vm.meses[greeting[key].id].fev = 0;
+						              vm.meses[greeting[key].id].mar = 0;
+						              vm.meses[greeting[key].id].abr = 0;
+						              vm.meses[greeting[key].id].mai = 0;
+						              vm.meses[greeting[key].id].jun = 0;
+						              vm.meses[greeting[key].id].jul = 0;
+						              vm.meses[greeting[key].id].ago = 0;
+						              vm.meses[greeting[key].id].set = 0;
+						              vm.meses[greeting[key].id].out = 0;
+						              vm.meses[greeting[key].id].nov = 0;
+						              vm.meses[greeting[key].id].dez = 0;
 						       		
 						       			var qq = greeting[key].numero;
 
@@ -73,54 +87,61 @@
 											       		
 											       		var dataciclo = new Date(greeting2[key2].data);
 											       	   
-											           console.log(greeting2[key2]);
+											           // console.log(greeting2[key2]);
 
 											           if (greeting2[key2].contribuinte) {
 											           		idcontrib = greeting2[key2].contribuinte.id;	
 											        
 											           	}
 
+
+											           	if (dataciclo.getFullYear() === vm.ano && typeof(greeting2[key2].valor) === 'string' )
+											           	{
+											           		var valornum = Number(greeting2[key2].valor);
+
+											           		// console.log(valornum)
+
 											             switch (dataciclo.getMonth()) {
 														   case 0:
-														     vm.meses[idcontrib].jan = greeting2[key2].valor ;
+														     vm.meses[idcontrib].jan +=  valornum ;
 														     break;
 														   case 1:
-														     vm.meses[idcontrib].fev = greeting2[key2].valor ;
+														     vm.meses[idcontrib].fev += valornum ;
 														     break;
 														   case 2:
-														     vm.meses[idcontrib].mar = greeting2[key2].valor ;
+														     vm.meses[idcontrib].mar += valornum ;
 														     break;
 														   case 3:
-														     vm.meses[idcontrib].abr = greeting2[key2].valor ;
+														     vm.meses[idcontrib].abr += valornum ;
 														     break;
 														   case 4:
-														     vm.meses[idcontrib].mai = greeting2[key2].valor ;
+														     vm.meses[idcontrib].mai += valornum;
 														     break;
 														   case 5:
-														     vm.meses[idcontrib].jun = greeting2[key2].valor ;
+														     vm.meses[idcontrib].jun += valornum ;
 														     break;
 														   case  6:
-														     vm.meses[idcontrib].jul = greeting2[key2].valor ;
+														     vm.meses[idcontrib].jul += valornum ;
 														     break;
 														   case  7:
-														     vm.meses[idcontrib].ago = greeting2[key2].valor ;
+														     vm.meses[idcontrib].ago += valornum;
 														     break;
 														   case  8:
-														     vm.meses[idcontrib].set = greeting2[key2].valor ;
+														     vm.meses[idcontrib].set += valornum;
 														     break;
 														   case  9:
-														     vm.meses[idcontrib].out = greeting2[key2].valor ;
+														     vm.meses[idcontrib].out += valornum;
 														     break;
 														   case  10:
-														     vm.meses[idcontrib].nov = greeting2[key2].valor ;
+														     vm.meses[idcontrib].nov += valornum;
 														     break;
 														   case  11:
-														     vm.meses[idcontrib].dez = greeting2[key2].valor ;
+														     vm.meses[idcontrib].dez += valornum;
 														     break;	
 
-														     console.log('mes '+dataciclo.getMonth()+' valor ')
-														     console.log(vm.meses[idcontrib])
+														     
 														 }
+														}
 
 													
 											       }
@@ -146,23 +167,128 @@
 			  
 			  $scope.getDesserts = function () {
 			    // $scope.promise = $scope.contribs.get($scope.query, success).$promise;
-				var query = {
-				  sort: 'nome',
-				  order: 'nome',
-				  limit: 30,
-				  page: 1
-				};
+				
 
-			    vm.contribs = Report1Service.query(query);
+			    vm.contribs = Report1Service.query(vm.query);
 
 			    vm.contribs.$promise.then(function(greeting) {
-					  // console.log('Success: ' + greeting);
+
+  // console.log('Success: ' + greeting);
+					  var pay = new Array;
 						for (var key in greeting) {
 						       if (greeting.hasOwnProperty(key)) {
+						       		if (greeting[key].id)
+						       		{
+						              // console.log(greeting[key].id);
+
+						              vm.meses[greeting[key].id] = [];
+						              vm.meses[greeting[key].id].jan = 0;
+						              vm.meses[greeting[key].id].fev = 0;
+						              vm.meses[greeting[key].id].mar = 0;
+						              vm.meses[greeting[key].id].abr = 0;
+						              vm.meses[greeting[key].id].mai = 0;
+						              vm.meses[greeting[key].id].jun = 0;
+						              vm.meses[greeting[key].id].jul = 0;
+						              vm.meses[greeting[key].id].ago = 0;
+						              vm.meses[greeting[key].id].set = 0;
+						              vm.meses[greeting[key].id].out = 0;
+						              vm.meses[greeting[key].id].nov = 0;
+						              vm.meses[greeting[key].id].dez = 0;
 						       		
-						             console.log(greeting[key]);
+						       			var qq = greeting[key].numero;
+
+							            var queryPay = {
+										  
+										  q: qq,
+										  limit: 30,
+										  page: 1
+										};
+
+										pay[key] = ReportPayment.query(queryPay);
+
+										pay[key].$promise.then(function(greeting2) {
+										  // console.log('Success: ' + greeting);
+										    var idcontrib;
+											for (var key2 in greeting2) {
+											       if (greeting2.hasOwnProperty(key2)) {
+											       		
+											       		var dataciclo = new Date(greeting2[key2].data);
+											       	   
+											           // console.log(greeting2[key2]);
+
+											           if (greeting2[key2].contribuinte) {
+											           		idcontrib = greeting2[key2].contribuinte.id;	
+											        
+											           	}
+
+
+											           	if (dataciclo.getFullYear() === vm.ano && typeof(greeting2[key2].valor) === 'string' )
+											           	{
+											           		var valornum = Number(greeting2[key2].valor);
+
+											           		// console.log(valornum)
+
+											             switch (dataciclo.getMonth()) {
+														   case 0:
+														     vm.meses[idcontrib].jan +=  valornum ;
+														     break;
+														   case 1:
+														     vm.meses[idcontrib].fev += valornum ;
+														     break;
+														   case 2:
+														     vm.meses[idcontrib].mar += valornum ;
+														     break;
+														   case 3:
+														     vm.meses[idcontrib].abr += valornum ;
+														     break;
+														   case 4:
+														     vm.meses[idcontrib].mai += valornum;
+														     break;
+														   case 5:
+														     vm.meses[idcontrib].jun += valornum ;
+														     break;
+														   case  6:
+														     vm.meses[idcontrib].jul += valornum ;
+														     break;
+														   case  7:
+														     vm.meses[idcontrib].ago += valornum;
+														     break;
+														   case  8:
+														     vm.meses[idcontrib].set += valornum;
+														     break;
+														   case  9:
+														     vm.meses[idcontrib].out += valornum;
+														     break;
+														   case  10:
+														     vm.meses[idcontrib].nov += valornum;
+														     break;
+														   case  11:
+														     vm.meses[idcontrib].dez += valornum;
+														     break;	
+
+														     
+														 }
+														}
+
+													
+											       }
+											} 
+
+										}, function(reason2) {
+										  console.log('Failed: ' + reason2);
+										});
+									}
+
+
 						       }
 						} 
+
+
+
+
+
+					  
+						
 
 					}, function(reason) {
 					  console.log('Failed: ' + reason);
